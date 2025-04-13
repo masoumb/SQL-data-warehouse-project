0a4ab8ea2cabd725e9308923d54be54b2db117d9
Masoumeh Bayat <144366398+masoumb@users.noreply.github.com>
2025-04-13 13:49:47 -0400

    Create proc_load_silver.sql

---
+++ script/silver/proc_load_silver.sql
@@ -0,0 +1,161 @@
+
+SQL-data-warehouse-project/script/silver/proc_load_silver
+
+/*
+===============================================================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===============================================================================================================
+Script Purpose: 
+    This stored procedure performs the ETL process to populate the silver schema tables from the bronze schema.
+  Actions:
+    - Truncate silver layer
+    - Insert cleaned and transformed data into silver from Bronze layer.
+  Prameters:
+    None.
+    This stored procedure does n't accept any parameters or returen any values.
+
+  Usage example:
+    EXE silver.load_silver;
+    ===========================================================================================================
+    */
+
+--EXEC bronze.load_bronze
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '=========================================================================='
+		PRINT 'Load Bronze Layer'
+		PRINT '=========================================================================='
+
+		PRINT '--------------------------------------------------------------------------'
+		PRINT 'Loading CRM Tables'
+		PRINT '--------------------------------------------------------------------------'
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_cust_info'
+		TRUNCATE TABLE bronze.crm_cust_info;
+		PRINT '>> Inserting Data Into: bronze.crm_cust_info'
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\Hamid Azizi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+		PRINT '>>----------------'
+		--Now we have to test the quality of bronze table
+		--SELECT * FROM bronze.crm_cust_info
+		--SELECT COUNT(*) FROM bronze.crm_cust_info
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_prd_info'
+		TRUNCATE TABLE bronze.crm_prd_info;
+		PRINT '>> Inserting Data Into: bronze.crm_prd_info'
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\Hamid Azizi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);			
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+		PRINT '>>----------------'
+		--Now we have to test the quality of bronze table
+		--SELECT * FROM bronze.crm_prd_info
+		--SELECT COUNT(*) FROM bronze.crm_prd_info
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_sales_details'
+		TRUNCATE TABLE bronze.crm_sales_details;
+		PRINT '>> Inserting Data Into: bronze.crm_sales_details'
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\Hamid Azizi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+		PRINT '>>----------------'
+		--Now we have to test the quality of bronze table----------------------------------------------
+		--SELECT * FROM bronze.crm_sales_details
+		--SELECT COUNT(*) FROM bronze.crm_sales_details
+	
+		PRINT '--------------------------------------------------------------------------'
+		PRINT 'Loading ERP Tables'
+		PRINT '--------------------------------------------------------------------------'
+	
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_cust_az12'
+		TRUNCATE TABLE bronze.erp_cust_az12;
+		PRINT '>> Inserting Data Into: bronze.erp_cust_az12'
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\Hamid Azizi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+		PRINT '>>----------------'
+		--Now we have to test the quality of bronze table----------------------------------------------
+		--SELECT * FROM bronze.erp_cust_az12
+		--SELECT COUNT(*) FROM bronze.erp_cust_az12
+
+		PRINT '>> Truncating Table: bronze.erp_loc_a101'
+		TRUNCATE TABLE bronze.erp_loc_a101;
+		PRINT '>> Inserting Data Into: bronze.erp_loc_a101'
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Users\Hamid Azizi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+		PRINT '>>----------------'
+		--Now we have to test the quality of bronze table----------------------------------------------
+		--SELECT * FROM bronze.erp_loc_a101
+		--SELECT COUNT(*) FROM bronze.erp_loc_a101
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2'
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2'
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\Hamid Azizi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+		PRINT '>>----------------'
+		--Now we have to test the quality of bronze table
+		--SELECT * FROM bronze.erp_px_cat_g1v2
+		--SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2
+		SET @batch_end_time = GETDATE();
+		PRINT '==========================================='
+		PRINT 'Loading Bronze Layer is Completed'
+		PRINT '    Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '==========================================='
+	END TRY
+	BEGIN CATCH
+		PRINT '==========================================='
+		PRINT 'Error Occured During Loading Bronze Layer'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '==========================================='
+	END CATCH
+END
